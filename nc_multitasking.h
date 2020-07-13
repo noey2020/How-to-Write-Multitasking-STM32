bb898eee83bf85014b95cf94f5deaa28b3ce54be
Noel S Cruz <65864827+noey2020@users.noreply.github.com>
2020-07-12 19:55:03 -0700

    Add files via upload

---
+++ nc_multitasking.h
@@ -0,0 +1,10 @@
+#include "stm32l1xx.h"
+	 
+	 typedef struct{
+    // pushed by software
+      unsigned int r4;
+		__IO uint32_t w;              /* main.c(3): error: unknown type name 'uint32_t'. #include "stm32l1xx.h" and declare extern or unsigned long int. */
+
+    // pushed by hardware
+
+} stack_frame_t;
